13d6b64b1a66b0494d2f371dc4be17a7ef5b176b
Cristian Visentini <cristianvisentini@hotmail.com>
2020-10-31 19:49:25 -0300

    Bug do banner resolvido

--- src/views/partials/right_side.php
+++ src/views/partials/right_side.php
@@ -1,13 +1,13 @@
 <div class="box banners">
     <div class="box-header">
-        <div class="box-header-text">Patrocinios</div>
+        <div class="box-header-text">Contato</div>
         <div class="box-header-buttons">
 
         </div>
     </div>
-    <div class="box-body">
-        <a href=""><img src="https://alunos.b7web.com.br/media/courses/php-nivel-1.jpg" /></a>
-        <a href=""><img src="https://alunos.b7web.com.br/media/courses/laravel-nivel-1.jpg" /></a>
+    <div class="box-body"> 
+        <a href="http://github.com/cristian-visentini" target="_blank"><img src="https://cdn.pixabay.com/photo/2017/08/05/11/24/logo-2582757_960_720.png" /></a>
+        <a href="https://www.linkedin.com/in/cristian-visentini/" target="_blank"><img src="https://cdn.pixabay.com/photo/2016/11/18/11/16/social-1834011_960_720.png" /></a>
     </div>
 </div>
 <div class="box">
